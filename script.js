3d523381dc8595e63c998765f1a1ffa8c18205df
Nidal Saffuri <101664120+nidalsaffuri1@users.noreply.github.com>
2023-11-06 18:04:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// document.querySelector(".message").textContent = "🎉 Correct answer";
+// document.querySelector(".label-score").textContent = "💯 Score: 30";
+// document.querySelector(".guess").value = "97";
+
+let rnd = Math.trunc(Math.random() * 20) + 1;
+console.log(rnd);
+let cnt = 20;
+let highScore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const valueGuess = Number(document.querySelector(".guess").value);
+
+  if (!valueGuess) {
+    document.querySelector(".message").textContent = "📛 no number!";
+  } else if (rnd === valueGuess) {
+    document.querySelector(".message").textContent = "🎉 Correct answer";
+    document.querySelector(".number").textContent = valueGuess;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    if (cnt > highScore) {
+      highScore = cnt;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (rnd > valueGuess) {
+    document.querySelector(".message").textContent = "Too Low!";
+    cnt--;
+    document.querySelector(".label-score").textContent = `💯 Score: ${cnt}`;
+  } else if (rnd < valueGuess) {
+    document.querySelector(".message").textContent = "Too High";
+    cnt--;
+    document.querySelector(".label-score").textContent = `💯 Score: ${cnt}`;
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  rnd = Math.trunc(Math.random() * 20) + 1;
+  cnt = 20;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".label-score").textContent = `💯 Score: ${cnt}`;
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+});
